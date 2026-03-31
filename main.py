e16e093c8bf627769bafcce501e68571bcea8b83
Elight-by <orsomedvedev.ilya@gmail.com>
2026-03-31 23:20:58 +0300

    Mini speed up

--- main.py
+++ main.py
@@ -24,72 +24,53 @@ def save_settings(new_data):
 
 settings_loaded = load_settings()
 
-byte = "bytes"
-kilo_byte = "kilobytes"
-mega_byte = "megabytes"
-giga_byte = "gigabytes"
-settings_first = "Select setting:\n1. Unit of measurement of information (Bytes, kilobytes, etc.)\n2. File name and other "
-settings_uomoi = "Select unit of measurement:\n1. Bytes\n2. Kilobytes (1024 bytes)\n3. Megabytes (1024 kilobytes)\n4. Gigabytes (1024 megabytes) "
-settings_uomoi_incorrect = "Incorrectly entered value! 1/2/3/4"
-settings_file_name = "Enter the file name with the type after the dot, for example: generated.data "
-generate = "Enter the file size in "
-succesfylly_generate = "File generated successfully"
-start_code = "1. Generate file\n2. Settings "
-
 
 def settings():
     settings_loaded = load_settings()
-    print(settings_first)
+    print("Select setting:\n1. Unit of measurement of information (Bytes, kilobytes, etc.)\n2. File name and other ")
     setting_get = input("1/2 ")
     if setting_get == "1":
-        print(settings_uomoi)
+        print("Select unit of measurement:\n1. Bytes\n2. Kilobytes (1024 bytes)\n3. Megabytes (1024 kilobytes)\n4. Gigabytes (1024 megabytes) ")
         uomoi = int(input("1/2/3/4 "))
         if uomoi not in (1, 2, 3, 4):
-            print(settings_uomoi_incorrect)
+            print("Incorrectly entered value! 1/2/3/4")
         else:
             settings_loaded["settings"]["uomoi"] = uomoi
             save_settings(settings_loaded)
     elif setting_get == "2":
-        print(settings_file_name)
+        print("Enter the file name with the type after the dot, for example: generated.data ")
         file_name = input()
         settings_loaded["settings"]["file_name"] = file_name
         save_settings(settings_loaded)
 
 
 def main():
-    now = 0
-    percent = 0
     settings_loaded = load_settings()
     file_name=settings_loaded["settings"]["file_name"]
     path_generate = os.path.join(current_directory, file_name)
     if settings_loaded["settings"]["uomoi"] == 1:
         one = 1
-        size_uomoi = byte
+        size_uomoi = "bytes"
     elif settings_loaded["settings"]["uomoi"] == 2:
         one = 1024
-        size_uomoi = kilo_byte
+        size_uomoi = "kilobytes"
     elif settings_loaded["settings"]["uomoi"] == 3:
         one = 1048576
-        size_uomoi = mega_byte
+        size_uomoi = "megabytes"
     elif settings_loaded["settings"]["uomoi"] == 4:
         one = 1073741824
-        size_uomoi = giga_byte
-    print(f"{generate} {size_uomoi}:")
+        size_uomoi = "gigabytes"
+    print(f"Enter the file size in {size_uomoi}:")
     file_hg = int(input())
     file_hg = file_hg * one
-    one_percent = file_hg/100
     with open(path_generate, 'w', encoding='utf-8') as file:
+        print("Starting...")
         for i in range(0, file_hg):
-            now = now + 1
             file.write("1")
-            if now >= one_percent:
-                percent = percent + 1
-                now = 0
-                print(f"{percent}%")
-        print(succesfylly_generate)
+    print("File generated successfully")
 
 def start():
-    start_choice = input(start_code)
+    start_choice = input("1. Generate file\n2. Settings ")
     if start_choice == "1":
         main()
     elif start_choice == "2":
